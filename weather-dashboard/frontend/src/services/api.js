bd9947144b34f11385e70cedf26e92d8122ae123
lohithkumar1810 <lohithkumar1810@gmail.com>
2026-03-12 23:37:45 -0700

    Update api.js

--- weather-dashboard/frontend/src/services/api.js
+++ weather-dashboard/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with production URL later
+  baseURL: 'https://weather-checker-mfhq.onrender.com', // Replace with production URL later
   headers: {
     'Content-Type': 'application/json'
   }
